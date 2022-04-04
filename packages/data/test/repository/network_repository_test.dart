b757f748b6f4d4e601d712fe85d6dd21ae754b33
Sergei Mikhailovskii <uSQcBQpfxPqTLDiwutWvSX>
2022-04-04 13:52:02 +0200

    Added unit test for success get registration

---
+++ packages/data/test/repository/network_repository_test.dart
@@ -0,0 +1,54 @@
+import 'package:data/repository/network_repository.dart';
+import 'package:data/service/api_service.dart';
+import 'package:dio/dio.dart';
+import 'package:domain/model/registration_response.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'network_repository_test.mocks.dart';
+
+/// flutter packages pub run build_runner build lib
+@GenerateMocks([ApiService, CancelToken])
+void main() {
+  group(
+    'getRegistration',
+    () {
+      test(
+        'returns an ApiRegistrationResponse if the api call completes successfully',
+        () async {
+          final cancelToken = MockCancelToken();
+          final apiService = MockApiService();
+          final repository = NetworkRepository(apiService, cancelToken);
+
+          when(apiService.get(
+            path: 'mxcc-registration/gateway/REGISTRATION/',
+            cancelToken: cancelToken,
+          )).thenAnswer(
+            (realInvocation) async => Response(
+              data: """
+          {
+    "step": "STEP_CONTACT",
+    "data": {
+        "contact": {
+            "phone": null,
+            "email": null
+        }
+    }
+}"""
+                  .trim(),
+              requestOptions: RequestOptions(
+                path: '',
+              ),
+            ),
+          );
+
+          expect(
+            await repository.getRegistration(),
+            isA<RegistrationResponse>(),
+          );
+        },
+      );
+    },
+  );
+}
